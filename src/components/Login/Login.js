c8031e9c57c7daae06513f008d4a0cecba2926a4
20424101-LeTuanKiet <20424101@student.hcmus.edu.vn>
2023-07-02 21:31:38 +0700

    delete login with google

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -36,7 +36,7 @@ function Login() {
                     <Title className={cx('title')} level={3}>
                         Chat Room
                     </Title>
-                    <Button className={cx('login-btn')}>Đăng nhập bằng Google</Button>
+                    {/* <Button className={cx('login-btn')}>Đăng nhập bằng Google</Button> */}
                     <Button className={cx('login-btn')} onClick={handleFbLogin}>
                         Đăng nhập bằng Facebook
                     </Button>
